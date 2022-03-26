07cda72932b9c05de18aaaf9ebd365ea51a1d82d
Syam Iyer <iyersyam@gmail.com>
2022-03-26 11:41:27 +1100

    Return the saved link

---
+++ src/link/link.service.ts
@@ -0,0 +1,11 @@
+import { Link } from "./link.entity";
+
+/**
+ * Save a link into storage
+ * @param link
+ * @returns the saved link
+ */
+export function saveLink(link: Link): Link {
+  // TODO: Save this to database
+  return link;
+}
