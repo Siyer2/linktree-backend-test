74dc8ab565165e9f4e9b75989e722b3dda12d032
Syam Iyer <iyersyam@gmail.com>
2022-03-26 12:45:43 +1100

    Made some properties in Link entity required

--- src/link/link.entity.ts
+++ src/link/link.entity.ts
@@ -1,33 +1,36 @@
 import { User } from "../user/user.entity";
 
-enum LinkType {
+export enum LinkType {
   Classic = "classic",
-  ShowsList = "showsList",
+  ShowList = "showsList",
   MusicPlayer = "musicPlayer",
 }
 
 export interface LinkTypeSpecificData {
+  title: string;
   redirectLink: string;
 }
 
 export class Link {
-  id: string;
+  id?: string;
   redirectLink?: string;
-  title: string;
+  title?: string;
   dateCreated: Date;
   user: User;
   linkType: LinkType;
   linkTypeSpecificData?: LinkTypeSpecificData[];
 
-  constructor(input: {
-    id: string;
-    redirectLink?: string;
-    title: string;
-    dateCreated: Date;
-    user: User;
-    linkType: LinkType;
-    linkTypeSpecificData?: LinkTypeSpecificData[];
-  }) {
+  constructor(input: Partial<Link>) {
+    if (!input.dateCreated) {
+      throw new Error("input.dateCreated cannot be undefined");
+    }
+    if (!input.user) {
+      throw new Error("input.user cannot be undefined");
+    }
+    if (!input.linkType) {
+      throw new Error("input.linkType cannot be undefined");
+    }
+
     this.id = input.id;
     this.redirectLink = input.redirectLink;
     this.title = input.title;
