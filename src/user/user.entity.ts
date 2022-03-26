e71930124a6188fd7712b7a6e05c5c15a1708c0b
Syam Iyer <iyersyam@gmail.com>
2022-03-26 12:46:40 +1100

    Made some properties in user entity required

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,10 +1,17 @@
 export class User {
-  id: string;
+  id?: string;
   firstName: string;
   lastName: string;
 
-  constructor(input: { id: string; firstName: string; lastName: string }) {
+  constructor(input: Partial<User>) {
     this.id = input.id;
+
+    if (!input.firstName) {
+      throw new Error("input.firstName cannot be undefined");
+    }
+    if (!input.lastName) {
+      throw new Error("input.lastName cannot be undefined");
+    }
     this.firstName = input.firstName;
     this.lastName = input.lastName;
   }
